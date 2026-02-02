0353d98fbb3179f10a59596edbf08e2f80f9090a
ilaysali <ilaysali123@gmail.com>
2026-02-02 19:30:13 +0200

    added constans static class to hold all constants for generic code

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudoku
+{
+    public static class Constants
+    {
+        public static int Size = 9;
+        public const int EmptyCell = 0;
+        public static readonly int BlockSize = (int)Math.Sqrt(Size);
+    }
+}
